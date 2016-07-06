10e6021b0386688ee0497e1c49ea16c25aa68eea
Anahi Salgado <anahisalgado@MacBook-Pro-de-Anahi.local>
2016-07-06 09:38:40 -0500

    implemntacion de icono de la app

--- HolaMundo/app/src/main/java/com/anncode/holamundo/MainActivity.java
+++ HolaMundo/app/src/main/java/com/anncode/holamundo/MainActivity.java
@@ -1,7 +1,13 @@
 package com.anncode.holamundo;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e("CREATE", getString(R.string.mensaje_oncreate));
+        Toast.makeText(MainActivity.this, getString(R.string.mensaje_oncreate), Toast.LENGTH_SHORT).show();
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e("START", "Método onStrat()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("RESUME", "Método onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("PAUSA", "Método onPausa()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("STOP", "Método onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("DESTROY", "Método onDestroy");
     }
 }
